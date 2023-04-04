269536b5c5662e4bb7103288c3f3b968768f63ef
nirjarigandhi <nirjari.gandhi@mail.utoronto.ca>
2023-04-04 09:53:52 -0400

    Update createDatasets.py

--- createDatasets.py
+++ createDatasets.py
@@ -6,6 +6,7 @@ def createDataset(filenames, dataset_size=50):
         # Open the file
         main_dataset = []
         curr_dataset = []
+        unique_words = set()
         for filename in filenames:
             f = open(filename, 'r')
             for line in f:
@@ -17,17 +18,35 @@ def createDataset(filenames, dataset_size=50):
                     if len(curr_dataset) == dataset_size:
                         main_dataset.append(curr_dataset)
                         curr_dataset = []
-                    else:
+                    if word != '':
                         curr_dataset.append(word)
+                        unique_words.add(word)
                 # Create a dataset from the list
                 # Add the dataset to the list
             f.close()
         main_dataset.append(curr_dataset)
-        return main_dataset
+        return main_dataset, unique_words
 
-dataset1 = createDataset(['play1.txt', 'play2.txt'])
+def createOneHotDataset(dataset, unique_words):
+    # Create a one hot dataset from a dataset
+    # Get the unique words
+    # Create a one hot dataset
+    # Return the one hot dataset
+    unique_words = list(unique_words)
+    one_hot_dataset = []
+    for data in dataset:
+        one_hot_data = []
+        for word in data:
+            one_hot_word = [0] * len(unique_words)
+            one_hot_word[unique_words.index(word)] = 1
+            one_hot_data.append(one_hot_word)
+        one_hot_dataset.append(one_hot_data)
+    return one_hot_dataset
+
+dataset1, unique_words = createDataset(['play1.txt', 'play2.txt'])
+dataset1 = createOneHotDataset(dataset1, unique_words)
 train_len = int(len(dataset1) * 0.8)
 train_dataset = dataset1[:train_len]
 test_len = int(len(dataset1) * 0.1)
-test_dataset = dataset1[train_len:test_len]
+test_dataset = dataset1[train_len:test_len+train_len]
 valid_dataset = dataset1[test_len+train_len:]
