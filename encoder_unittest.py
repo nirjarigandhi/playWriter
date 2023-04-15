b03267cbf07eae471bea40c5cf633887b09c7ddd
vijay <vijay@pop-os.localdomain>
2023-04-15 16:08:52 -0400

    updated forward method

--- encoder_unittest.py
+++ encoder_unittest.py
@@ -6,8 +6,8 @@ class TestEncoder(unittest.TestCase):
 
     def test_general_run(self):
         inputs = torch.rand((2, 5, 16))
-        en = EncoderLayer(inputs, 2, 5, 16, 4, 7, 20)
-        catch = en.forward()
+        en = EncoderLayer(16, 4, 3, 20)
+        catch = en.forward(inputs, 2, 5)
         print(catch)
         assert tuple(catch.shape) == tuple(inputs.shape)
 
@@ -15,4 +15,3 @@ class TestEncoder(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(verbosity=True)
-    
--- encoderlayer.py
+++ encoderlayer.py
@@ -10,26 +10,31 @@ class EncoderLayer(nn.Module):
     """This is the encoder layer it combines the attention plus the feed forward layer of the neural network
     Note that batch_size, sentence_length, and an embedding_size tensor goes into this class and a tensor of the same size
     comes out"""
-    def __init__(self, inputs: torch.Tensor, batch_size: int, sentence_length: int, embedding_size: int, heads: int, reduced_emb: int, hidden_dim_ff: int) -> None:
+    def __init__(self, embedding_size: int, heads: int, reduced_emb: int, hidden_dim_ff: int) -> None:
         super(EncoderLayer, self).__init__()
-        self.inputs = inputs
-        self.batch_size = batch_size
-        self.sentence_length = sentence_length
+        self.inputs = None
+        self.batch_size = None
+        self.sentence_length = None
         self.embedding_size = embedding_size
         self.heads = heads
         self.reduced_emb = reduced_emb
-        self.multiattention = MultiHeadAttention(self.heads, self.inputs, self.inputs, self.batch_size, self.sentence_length, self.embedding_size, self.batch_size, self.sentence_length, self.embedding_size, self.reduced_emb)
+        self.hidden_dim_ff = hidden_dim_ff
+        self.multiattention = MultiHeadAttention(self.heads, self.embedding_size, self.embedding_size, self.reduced_emb)
         self.feed_forward = FeedForward(self.embedding_size, hidden_dim_ff, self.embedding_size)
         self.w0 = torch.rand((1, self.reduced_emb * self.heads, self.embedding_size), requires_grad=True) # remember that batch_kv = batch_q and in this instance all q,k,v tensors share the same dimensions. What comes out of the multi head attentions is (batch_kv, seq_q, reduced_emb * self.heads)
         torch.nn.init.xavier_normal_(self.w0)
         self.result = None
 
-        
-
+    def update(self, inputs: torch.Tensor, batch_size: int, sentence_length: int):
+        self.inputs = inputs
+        self.batch_size = batch_size
+        self.sentence_length = sentence_length
+        self.result = None
     
-    def forward(self):
+    def forward(self, inputs: torch.Tensor, batch_size: int, sentence_length: int):
+        self.update(inputs, batch_size, sentence_length)
         store1 = self.inputs.clone()
-        out = self.multiattention.forward()
+        out = self.multiattention.forward(self.inputs, self.inputs, self.batch_size, self.sentence_length, self.batch_size, self.sentence_length)
         out = torch.matmul(out, self.w0) # will now have shape (batch_kv = batch = batch_q, sentence_length = seq_q = seq_kv, embedding_size)
         second = store1 + out
         second = torch.nn.functional.normalize(second, 2, dim=0)
