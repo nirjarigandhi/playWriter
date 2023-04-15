bfc8b35d838035349090981e604f521abfdbb93b
vijay <vijay@pop-os.localdomain>
2023-04-15 15:58:10 -0400

    forward redesign

--- attention.py
+++ attention.py
@@ -9,20 +9,21 @@ class SingleAttention(nn.Module):
     It will take a matrix of (batch_size, sentence_lenght, embedding_size)
     batch_size, sentence_length, and embedding size, are all sizes that are fed into this class
     these sizes speficically embedding size can be manipulated by linear layers before entering this class.
-    Reduced emb is a variable specific to each query, value, and key matrix"""
-    def __init__(self, inputs_kv: torch.Tensor, inputs_q:torch.Tensor ,batch_size_kv: int, sentence_length_kv: int, embedding_size_kv: int, batch_size_q:int, sentence_length_q: int, embedding_size_q: int,reduced_emb: int, mask=None) -> None:
+    Reduced emb is a variable specific to each query, value, and key matrix
+    
+    NOTE that embedding_size and reduce emb are fixed for every tensor matrix that enters the encoder they cannot change regardless of the data coming through"""
+    def __init__(self, embedding_size_kv: int, embedding_size_q: int, reduced_emb: int, mask=None) -> None:
         super(SingleAttention, self).__init__()
-        assert batch_size_kv == batch_size_q, "The batch sizes of both the kv and q inputs must be equal"
-        self.inputs_kv = inputs_kv #Has the shape (batch_kv, sentence_length_kv, embedding_kv)
-        self.inputs_q = inputs_q
-        self.batch_size_kv = batch_size_kv
-        self.sentence_length_kv = sentence_length_kv
+        self.inputs_kv = None #Has the shape (batch_kv, sentence_length_kv, embedding_kv)
+        self.inputs_q = None
+        self.batch_size_kv = None
+        self.sentence_length_kv = None
         self.embedding_size_kv = embedding_size_kv
-        self.batch_size_q = batch_size_q
+        self.batch_size_q = None
         self.embedding_size_q = embedding_size_q
-        self.sentence_length_q = sentence_length_q
+        self.sentence_length_q = None
         self.reduced_emb = reduced_emb
-        self.mask = mask #This is used to 0 out certain values so that sentences cannot look ahead when making preditions in the decoder
+        self.mask = mask
         self.query_matrix = torch.rand((1, self.embedding_size_q, self.reduced_emb), requires_grad=True) # The input vector is of size (seq, batch, emb) input * self.query_vector
         torch.nn.init.xavier_normal_(self.query_matrix, gain=3.5)
         self.key_matrix = torch.rand((1, self.embedding_size_kv, self.reduced_emb), requires_grad=True)
@@ -34,6 +35,23 @@ class SingleAttention(nn.Module):
         self.values = None
         self.attention_matrix = None #This is of shape (batch_q = batch_kv, sentence_q, sentence_kv)
         self.final = None
+    
+    def update(self, inputs_kv: torch.Tensor, inputs_q:torch.Tensor ,batch_size_kv: int, sentence_length_kv: int, batch_size_q:int, sentence_length_q: int, mask=None):
+        """Changes the values that need to be changed with every new set of data flowing through the prediction network"""
+        assert batch_size_kv == batch_size_q, "The batch sizes of both the kv and q inputs must be equal"
+        self.inputs_kv = inputs_kv #Has the shape (batch_kv, sentence_length_kv, embedding_kv)
+        self.inputs_q = inputs_q
+        self.batch_size_kv = batch_size_kv
+        self.sentence_length_kv = sentence_length_kv
+        self.batch_size_q = batch_size_q
+        self.sentence_length_q = sentence_length_q
+        self.mask = mask
+        self.querys = None
+        self.keys = None
+        self.values = None
+        self.attention_matrix = None #This is of shape (batch_q = batch_kv, sentence_q, sentence_kv)
+        self.final = None
+
 
     def _get_querys(self):
         """Get query associate with each word". This is a right multiplication for the
@@ -82,7 +100,8 @@ class SingleAttention(nn.Module):
 
         return final
     
-    def forward(self):
+    def forward(self, inputs_kv: torch.Tensor, inputs_q:torch.Tensor ,batch_size_kv: int, sentence_length_kv: int,  batch_size_q:int, sentence_length_q: int, mask=None):
+        self.update(inputs_kv, inputs_q, batch_size_kv, sentence_length_kv, batch_size_q, sentence_length_q, mask)
         self._get_querys()
         self._get_keys()
         self._get_values()
@@ -95,34 +114,42 @@ class MultiHeadAttention(nn.Module):
     """This class implements multihead attention using smaller single head attention modules. The main work being done
     here is splitting the inputs by their embedding and creating heads amount of single attention classes to give those split workloads too.
     Remember that the input tensors should have format (batch, sentence_length, embedding_size ) and so should the output tensors"""
-    def __init__(self, heads: int, inputs_kv: torch.Tensor, inputs_q:torch.Tensor ,batch_size_kv: int, sentence_length_kv: int, embedding_size_kv: int, batch_size_q:int, sentence_length_q: int, embedding_size_q: int,reduced_emb: int, mask=None) -> None:
+    def __init__(self, heads: int, embedding_size_kv: int, embedding_size_q: int,reduced_emb: int, mask=None) -> None:
         super(MultiHeadAttention, self).__init__()
         assert (embedding_size_kv / heads == embedding_size_kv // heads), "The embedding size of the key and value matrices must be divisible by number of heads"
         assert (embedding_size_q / heads == embedding_size_q // heads), "The embedding size of the querys must be divisble by the number of heads"
-        self.inputs_kv = inputs_kv
-        self.inputs_q = inputs_q
-        self.batch_size_kv = batch_size_kv
-        self.sentence_length_kv = sentence_length_kv # when placed into the single attention classes this will be divied by heads
+        self.inputs_kv = None
+        self.inputs_q = None
+        self.batch_size_kv = None
+        self.sentence_length_kv = None 
         self.embedding_size_kv = embedding_size_kv # when placed into the single attention classes this will be divided by heads
-        self.batch_size_q = batch_size_q
+        self.batch_size_q = None
         self.embedding_size_q = embedding_size_q
-        self.sentence_length_q = sentence_length_q
+        self.sentence_length_q = None
         self.reduced_emb = reduced_emb #Remeber that reduced emb is not the split that happens with mha but one of the dims of the query, value, and key matrices that occur in the single attention layers
         self.heads = heads
         self.mask = mask
         self.output_tensor = None
+        self.attention_list = [SingleAttention(self.embedding_size_kv // self.heads , self.embedding_size_q // self.heads , self.reduced_emb, self.mask) for i in range(self.heads)]
+
+    def update(self, inputs_kv: torch.Tensor, inputs_q:torch.Tensor ,batch_size_kv: int, sentence_length_kv: int, batch_size_q:int, sentence_length_q: int, mask=None):
+        self.inputs_kv = inputs_kv
+        self.inputs_q = inputs_q
+        self.batch_size_kv = batch_size_kv
+        self.sentence_length_kv = sentence_length_kv
+        self.batch_size_q = batch_size_q
+        self.sentence_length_q = sentence_length_q
+        self.mask = mask
+        self.output_tensor = None
     
-    def forward(self):
-        attention_list = [None for i in range(self.heads)]
+    def forward(self, inputs_kv: torch.Tensor, inputs_q:torch.Tensor ,batch_size_kv: int, sentence_length_kv: int, batch_size_q:int, sentence_length_q: int, mask=None):
+        self.update(inputs_kv, inputs_q, batch_size_kv, sentence_length_kv, batch_size_q, sentence_length_q, mask)
         list_of_input_kvs =torch.split(self.inputs_kv, self.embedding_size_kv // self.heads, 2)
         list_of_input_qs = torch.split(self.inputs_q, self.embedding_size_q // self.heads, 2)
         outputs = [None for i in range(self.heads)]
 
         for i in range(self.heads):
-            attention_list[i] = SingleAttention(list_of_input_kvs[i], list_of_input_qs[i], self.batch_size_kv, self.sentence_length_kv, self.embedding_size_kv // self.heads, self.batch_size_q, self.sentence_length_q, self.embedding_size_q // self.heads, self.reduced_emb, self.mask)
-
-        for i in range(self.heads):
-            outputs[i] = attention_list[i].forward()
+            outputs[i] = self.attention_list[i].forward(list_of_input_kvs[i], list_of_input_qs[i], self.batch_size_kv, self.sentence_length_kv, self.batch_size_q, self.sentence_length_q, mask)
         
         result = torch.concat(outputs, 2) # This should have size (batch_q = batch_kv, sentence_length_q, reduced_emb * self.heads)
         self.output_tensor = result.clone()
--- attention_unittest.py
+++ attention_unittest.py
@@ -6,21 +6,27 @@ class TestAttention(unittest.TestCase):
 
     def test_passthrough_singlehead(self):
         y = torch.rand((2, 3, 8))
-        ma = MultiHeadAttention(1, y, y, 2, 3, 8, 2, 3, 8, 5, None)
-        catch = ma.forward()
+        ma = MultiHeadAttention(1, 8, 8, 5, None)
+        catch = ma.forward(y, y, 2, 3, 2, 3, None)
         assert tuple(catch.shape) == (2, 3, 5)
     
     def test_passthrough_doublehead(self):
         y = torch.rand((2, 3, 8))
-        ma = MultiHeadAttention(2, y, y, 2, 3, 8, 2, 3, 8, 5, None)
-        catch = ma.forward()
+        ma = MultiHeadAttention(2, 8, 8, 5, None)
+        catch = ma.forward(y, y, 2, 3, 2, 3, None)
         assert tuple(catch.shape) == (2, 3, 10)
     
     def test_encoder_decoder_pass(self):
         y = torch.rand((2, 5, 16))
         x = torch.rand((2, 3, 8))
-        ma = MultiHeadAttention(2, y, x, 2, 5, 16, 2, 3, 8, 5, None)
-        catch = ma.forward()
+        ma = MultiHeadAttention(2, 16, 8, 5, None)
+        catch = ma.forward(y, x, 2, 5, 2, 3, None)
+        assert tuple(catch.shape) == (2, 3, 10)
+
+
+        y = torch.rand((2, 5, 32))
+        x = torch.rand((2, 3, 8))
+        catch = ma.forward(y, x, 2, 5, 2, 3, None)
         assert tuple(catch.shape) == (2, 3, 10)
     
     def test_random(self):
@@ -29,10 +35,10 @@ class TestAttention(unittest.TestCase):
         y_embedding_Size = 20
         heads = 4
         reduced_emb = 4
-        y = torch.rand(y_batch, y_seqlen, y_embedding_Size)
-        x = torch.rand(y_batch, 8, 16)
-        ma = MultiHeadAttention(heads, x, y, y_batch, 8, 16, y_batch, y_seqlen, y_embedding_Size, reduced_emb, None)
-        catch = ma.forward()
+        y = torch.rand(y_batch, y_seqlen, y_embedding_Size) #decoder input kv
+        x = torch.rand(y_batch, 8, 16) #encoder inputkv
+        ma = MultiHeadAttention(heads, 16, y_embedding_Size, reduced_emb, None)
+        catch = ma.forward(x, y, y_batch, 8, y_batch, y_seqlen, None)
         assert tuple(catch.shape) == (y_batch, y_seqlen ,reduced_emb * heads)
 
     
