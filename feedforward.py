a56c780e605cb08d31591f879369d723211428a5
Vijay Nandlal <vijaynandlal11@gmail.com>
2023-04-15 08:56:02 -0400

    fixed incorrect use of relu in the feedforward layer

--- feedforward.py
+++ feedforward.py
@@ -11,14 +11,13 @@ class FeedForward(nn.Module):
         self.output_dim = output_dim
         self.hidden_dim = hidden_dim
         self.first = nn.Linear(self.input_dim, self.output_dim, bias=True)
-        self.second = torch.relu(self.first)
         self.last = nn.Linear(self.hidden_dim, self.output_dim, bias=True)
         self.result = None
 
     def forward(self, input_tensor: torch.Tensor):
 
         self.result = self.first(input_tensor)
-        self.result = self.second(self.result)
+        self.result = torch.relu(self.result)
         self.result = self.last(self.result)
 
         return self.result
