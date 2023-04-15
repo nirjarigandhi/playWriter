b1696af2223a96a63ae97ec82b609568da6a5414
Vijay Nandlal <vijaynandlal11@gmail.com>
2023-04-15 09:51:10 -0400

    added another attention test

--- attention_unittest.py
+++ attention_unittest.py
@@ -1,5 +1,6 @@
 import unittest
 from attention import *
+from random import randint
 
 class TestAttention(unittest.TestCase):
 
@@ -21,6 +22,19 @@ class TestAttention(unittest.TestCase):
         ma = MultiHeadAttention(2, y, x, 2, 5, 16, 2, 3, 8, 5, None)
         catch = ma.forward()
         assert tuple(catch.shape) == (2, 3, 10)
+    
+    def test_random(self):
+        y_batch = randint(4, 10)
+        y_seqlen = randint(8, 20)
+        y_embedding_Size = 20
+        heads = 4
+        reduced_emb = 4
+        y = torch.rand(y_batch, y_seqlen, y_embedding_Size)
+        x = torch.rand(y_batch, 8, 16)
+        ma = MultiHeadAttention(heads, x, y, y_batch, 8, 16, y_batch, y_seqlen, y_embedding_Size, reduced_emb, None)
+        catch = ma.forward()
+        assert tuple(catch.shape) == (y_batch, y_seqlen ,reduced_emb * heads)
+
     
 
 
