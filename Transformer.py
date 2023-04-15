94d2a9999e891eaa3a9a6b6174152b9b9dd23661
Vijay Nandlal <vijaynandlal11@gmail.com>
2023-04-15 18:22:13 -0400

    almost complete

--- Transformer.py
+++ Transformer.py
@@ -3,14 +3,47 @@ from decoderlayer import *
 from Posencoding import *
 
 class Transformer(nn.Module):
-    def __init__(self, encoder_amount: int, encoder_head: int, encoder_embedding: int, encoder_reduced_emb: int, encoder_hidden_ff: int, decoder_amount: int, decoder_head_first: int, decoder_head_second: int, decoder_embedding: int, decoder_reduced_emb_first: int, decoder_reduced_emb_second: int, decode_mask = None) -> None:
+    def __init__(self, encoder_amount: int, encoder_head: int, encoder_embedding: int, encoder_reduced_emb: int, encoder_hidden_ff: int, decoder_amount: int, decoder_head_first: int, decoder_head_second: int, decoder_embedding: int, decoder_reduced_emb_first: int, decoder_reduced_emb_second: int, onehot_embedding_size: int, onehot , decode_mask = None,) -> None:
+        """encoder_embedding - The embedding size of the data for the encoder and may or may not be the same as decoder embedding
+           encoder_amount    - The amount of encoder layers to pass through
+           encoder_head      - The amount of multihead attention to give the encoder attention layer
+           encoder_reduced_emb - The amount to reduce the q, v, k word representaions to from their original form"""
         super(Transformer, self).__init__()
         assert encoder_amount > 0, "The encoder layers need to be greater than 0"
         assert decoder_amount > 0, "The decoder layers need to be greater than 0"
+        self.encoder_amount = encoder_amount
+        self.encoder_embedding = encoder_embedding
+        self.decoder_amount = decoder_amount
         self.encoders = [EncoderLayer(encoder_embedding, encoder_head, encoder_reduced_emb, encoder_hidden_ff) for i in range(encoder_amount)]
         self.decoders = [DecoderLayer(encoder_embedding, decoder_embedding, decoder_head_first, decoder_reduced_emb_first, decoder_head_second, decoder_reduced_emb_second, decode_mask) for i in range(decoder_amount)]
-        # self.input_one_hot_mutate = torch.rand(())
+        self.input_one_hot_mutate = torch.rand((1, onehot_embedding_size, encoder_embedding), requires_grad=True)
+        torch.nn.init.xavier_normal_(self.input_one_hot_mutate, 5)
+        self.posencode = PositionalEmbeddings()
 
 
-    def forward(self):
-        pass
+
+    def forward(self, encoder_onehot_inputs_raw_tensor: torch.Tensor, decoder_onehot_inputs_raw_tensor: torch.Tensor):
+        encoder_inputs = torch.matmul(encoder_onehot_inputs_raw_tensor, self.input_one_hot_mutate)
+        encoder_inputs = self.posencode.add_posencoding(encoder_inputs, self.encoder_embedding, tuple(encoder_inputs.shape)[1], tuple(encoder_inputs.shape)[0])
+        # Convert both the onehot vectors fed to the network from both the encoder and decoder
+        decoder_inputs = torch.matmul(decoder_onehot_inputs_raw_tensor, self.input_one_hot_mutate)
+        decoder_inputs = self.posencode.add_posencoding(decoder_inputs, self.encoder_embedding, tuple(decoder_inputs.shape)[1], tuple(decoder_inputs.shape)[0])
+
+        assert tuple(encoder_onehot_inputs_raw_tensor.shape)[0] == tuple(decoder_onehot_inputs_raw_tensor.shape)[0], "The batch sizes of the encoder and decoder tensors must be equal!"
+
+        #Compute encoder pass
+        encoder_allpass_outputs = encoder_inputs
+
+        for i in range(self.encoder_amount):
+            encoder_allpass_outputs = self.encoders[i].forward(encoder_allpass_outputs, tuple(encoder_allpass_outputs.shape)[0], tuple(encoder_allpass_outputs.shape)[1])
+
+        
+
+        decoder_allpass_outputs = decoder_inputs
+        for i in range(self.decoder_amount):
+            decoder_allpass_outputs = self.decoders[i].forward(encoder_allpass_outputs, encoder_allpass_outputs[0], encoder_allpass_outputs[1], decoder_allpass_outputs, tuple(decoder_allpass_outputs)[0], tuple(decoder_allpass_outputs)[1], None)
+
+        
+        
+
+        
