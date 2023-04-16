a3363fd1f5b57f43ef320889e5c79094e570ec75
vijay <vijay@pop-os.localdomain>
2023-04-16 14:31:07 -0400

    broken

--- createDatasets.py
+++ createDatasets.py
@@ -25,6 +25,8 @@ def createDataset(filenames, dataset_size=50):
                 # Create a dataset from the list
                 # Add the dataset to the list
             f.close()
+        ## Add decoder <start> 
+        unique_words.add("<start>")
         main_dataset.append(curr_dataset)
         return main_dataset, unique_words
 
@@ -45,6 +47,8 @@ def createOneHotDataset(dataset, unique_words):
     return one_hot_dataset
 
 dataset1, unique_words = createDataset(['play1.txt', 'play2.txt'])
+unique_words = list(unique_words)
+start_token = unique_words.index("<start>")
 dataset1 = createOneHotDataset(dataset1, unique_words)
 train_len = int(len(dataset1) * 0.8)
 train_dataset = dataset1[:train_len]
@@ -58,20 +62,23 @@ test_dataset = torch.Tensor(test_dataset)
 valid_dataset = torch.Tensor(valid_dataset)
 
 
-device = torch.device("cuda:0")
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 print("Training", flush=True)
-x = train_dataset[1:30, :49, :].to(device)
-y = train_dataset[1:30, 49:, :].to(device)
-transformer = Transformer(6, 8, 512, 50, 1000, 6, 8, 8, 512, 50, 50, 10085, None).to(device)
+x = train_dataset[0:50, :49, :].to(device)
+y = train_dataset[0:50, 49:, :].to(device)
+transformer = Transformer(6, 8, 512, 50, 1000, 6, 8, 8, 512, 50, 50, 10086, None).to(device)
 loss_func = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(transformer.parameters(recurse=True))
+optimizer = torch.optim.Adam(transformer.parameters(recurse=True), lr=0.00001)
 optimizer.zero_grad()
 for i in range(200):
-    results = transformer.forward(x, y)
-    
-    loss = loss_func(y.transpose(1, 2).float(), transformer.pre_logit.transpose(1, 2).float())
+    de_in = torch.nn.functional.one_hot(torch.Tensor([start_token]).long(), 10086).unsqueeze(0) * torch.ones((50, 1, 10086))
+    results = transformer.forward(x, de_in.to(device))
+    print(f'Shape of output {results.shape}, shape of y {torch.argmax(y, 2).flatten().shape}', flush=True)
+
+    # loss = loss_func(y.reshape((y.shape[0] * y.shape[1], y.shape[2])), transformer.pre_logit.reshape((transformer.pre_logit.shape[0] * transformer.pre_logit.shape[1], transformer.pre_logit.shape[2])))
+    loss = loss_func(, torch.argmax(results, 2).flatten().float())
     loss.retain_grad()
     loss.backward()
-    print(loss.item(), flush=True)
+    print(loss.item())
     optimizer.step()
     optimizer.zero_grad()
