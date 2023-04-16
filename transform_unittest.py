39a64991b99cc23754a50ac548f3870c99af36e0
vijay <vijay@pop-os.localdomain>
2023-04-15 23:29:58 -0400

    small tester

---
+++ transform_unittest.py
@@ -0,0 +1,28 @@
+import unittest
+
+from Transformer import *
+
+class TestTransformer(unittest.TestCase):
+
+    def test_first(self):
+
+        words = torch.tensor([4, 5, 3, 2, 3, 4])
+        onehots = torch.Tensor(torch.nn.functional.one_hot(words, 7))
+        onehots= onehots.unsqueeze(0)
+        decode_in = torch.zeros((1, 1, 7))
+        decode_in[0, 0, 4] = 1
+
+        engine = Transformer(3, 2, 10, 5, 20, 3, 2, 2, 10, 5, 5, 7, None)
+
+        answers = engine.forward(onehots.float(), decode_in.float())
+
+        print(answers)
+        assert True
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=True)
